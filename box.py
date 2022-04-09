5e49fc6ebecd95b4d60be23ff88dfa6dd3f32261
veggielane <chris@lane.org.uk>
2022-04-09 10:03:56 +0100

    .

--- box.py
+++ box.py
@@ -2,5 +2,5 @@ import cadquery as cq
 
 result = cq.Workplane("XY" ).box(3, 3, 0.5).edges("|Z").fillet(0.125)
 
-result.val().exportStep("box.step")
-cq.exporters.export(result, 'box.stl')
+result.val().exportStep("output/box.step")
+cq.exporters.export(result, 'output/box.stl')

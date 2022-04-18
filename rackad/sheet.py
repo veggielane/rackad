d502ff18e33ecf8262f3ec7bd635c5ad3304df8e
veggielane <chris@lane.org.uk>
2022-04-18 19:31:10 +0100

    Update sheet.py

--- rackad/sheet.py
+++ rackad/sheet.py
@@ -1,6 +1,7 @@
 import cadquery as cq
 from typing import Callable
-import math 
+import math
+
 
 def flange(
     wp: cq.Workplane,
@@ -13,7 +14,7 @@ def flange(
 ) -> cq.Workplane:
     def _flange_callback(face):
         face_wp = cq.Workplane(face)
-        
+
         long_edge = edge_selector(face_wp).first()
         xaxis = long_edge.val().endPoint() - long_edge.val().startPoint()
         short_edge = face_wp.edges(cq.selectors.PerpendicularDirSelector(xaxis)).first()
@@ -21,19 +22,42 @@ def flange(
 
         center = face.Center()
         zaxis = -face.normalAt()
-
         if flip:
             xaxis = -xaxis
 
-        fwp = cq.Workplane(cq.Plane(center, xaxis, zaxis), origin=center, obj=face)
-        
-        bend = fwp.wires().toPending().revolve(angle, (-1, radius+thickness/2.0, 0), (1, radius+thickness/2.0, 0))
+        bend = (
+            cq.Workplane(cq.Plane(center, xaxis, zaxis), origin=center, obj=face)
+            .wires()
+            .toPending()
+            .revolve(
+                angle,
+                (-1, radius + thickness / 2.0, 0),
+                (1, radius + thickness / 2.0, 0),
+            )
+        )
 
-
-        bend = bend.faces(cq.selectors.AndSelector(cq.selectors.AndSelector(cq.selectors.InverseSelector(cq.selectors.ParallelDirSelector(xaxis)),cq.selectors.InverseSelector(cq.selectors.ParallelDirSelector(zaxis))),cq.selectors.TypeSelector("PLANE"))).tag("end").workplane().faces(tag="end").wires().toPending().extrude(distance)
-       
-        
-        return bend.val()
+        flat = (
+            bend.faces(
+                cq.selectors.AndSelector(
+                    cq.selectors.AndSelector(
+                        cq.selectors.InverseSelector(
+                            cq.selectors.ParallelDirSelector(xaxis)
+                        ),
+                        cq.selectors.InverseSelector(
+                            cq.selectors.ParallelDirSelector(zaxis)
+                        ),
+                    ),
+                    cq.selectors.TypeSelector("PLANE"),
+                )
+            )
+            .tag("end")
+            .workplane()
+            .faces(tag="end")
+            .wires()
+            .toPending()
+            .extrude(distance - thickness - radius)
+        )
+        return flat.val()
 
     return wp.union(face_selector(wp).each(_flange_callback))
 
@@ -42,10 +66,8 @@ cq.Workplane.flange = flange
 
 result = cq.Workplane("XY").box(10, 10, 1)
 result = result.flange(lambda wp: wp.faces(">Y"), lambda wp: wp.edges(">Z"), 90, 1, 5)
-result = result.flange(lambda wp: wp.faces("<Y"), lambda wp: wp.edges(">Z"), 90, 1, 5, True)
+result = result.flange(
+    lambda wp: wp.faces("<Y"), lambda wp: wp.edges(">Z"), 90, 1, 5, True
+)
 show_object(result)
-cq.exporters.export(result, 'c:/temp/sheet.step')
-# edge = face
-
-
-# result.flange()
+cq.exporters.export(result, "c:/temp/sheet.step")
